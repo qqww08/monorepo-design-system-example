447461457bf40b297130879e4b08fc6fee945108
Dan <qqwwee08@naver.com>
2022-07-16 17:50:47 +0900

    chore: package rename

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-qqww08`
-  extends: ["acme"],
+  extends: ["qqww08"],
   settings: {
     next: {
       rootDir: ["apps/*/"],
